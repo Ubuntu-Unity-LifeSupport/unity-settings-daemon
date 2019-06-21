dc76370d3d1e6e5c7e2c7a628716b0b1d78026e7
Khurshid Alam <khurshid.alam@linuxmail.org>
2019-06-21 10:30:34 +0200

    15.04.1+19.04.20190209-0ubuntu3.1 (patches unapplied)
    Imported using git-ubuntu import.

--- gnome-settings-daemon/gnome-settings-bus.h
+++ gnome-settings-daemon/gnome-settings-bus.h
@@ -30,6 +30,7 @@ G_BEGIN_DECLS
 
 GsdSessionManager        *gnome_settings_bus_get_session_proxy       (void);
 GsdScreenSaver           *gnome_settings_bus_get_screen_saver_proxy  (void);
+char *                    gnome_settings_get_chassis_type            (void);
 
 G_END_DECLS
 
